4d171f84f9c9a8c9e81abcce314b6425577a0d05
unknown <DIPeychev19@codingburgas.bg>
2024-03-18 23:08:56 +0200

    Add project

---
+++ FileExplorer/Models/FileModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+public class UploadFileModel
+{
+    [Required(ErrorMessage = "Please select a file.")]
+    [Display(Name = "File")]
+    public IFormFile File { get; set; }
+
+    public string FileContent { get; set; }
+}
